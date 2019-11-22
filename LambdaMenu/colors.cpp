4f2824d493b65deba0bac5b659703bef4aad9fbe
Zeemah <jhodgson1@protonmail.ch>
2019-11-22 21:25:25 +0000

    COLR: purple now

--- LambdaMenu/colors.cpp
+++ LambdaMenu/colors.cpp
@@ -1,4 +1,4 @@
 #include <string>
 
-int menuColor_RGB[3] = { 52, 181, 94 };
-std::string menuColor_HEX = "#34B55E";
+int menuColor_RGB[3] = { 117, 27, 241 };
+std::string menuColor_HEX = "#751BF1";
