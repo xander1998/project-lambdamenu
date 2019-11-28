b62360a3a17bf7c15fd3a23f41824acc07aefcde
Zeemah <jhodgson1@protonmail.ch>
2019-11-28 00:16:26 +0000

    ER: backward compat

--- LambdaMenu/database_sqlite.cpp
+++ LambdaMenu/database_sqlite.cpp
@@ -403,8 +403,11 @@ bool ERDatabaseSQL::open()
 
 	write_text_to_log_file("Opening DB file");
 
-
+#ifdef ER_DB
+	WCHAR* db_path = get_storage_dir_path("/er.db");
+#else
 	WCHAR* db_path = get_storage_dir_path("/lm.db");
+#endif
 
 	//std::wstring ws(db_path);
 	//std::string fileSS(ws.begin(), ws.end());
